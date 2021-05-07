c9c5d988206b9f9d5da41ffad72d8db8e8c3f0ab
pyin <pyin@vmware.com>
2021-05-07 18:34:44 +0800

    init

---
+++ Solution.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> t1_twoSum(vector<int>& nums, int target) {
+        }
+}
+
+int main() {
+    Solution s;
+    s.t1_twoSum();
+}
